59ab7649de6fc3e0c99ebca4b32a936f35baeb10
Cameron <cameron.beckit@gmail.com>
2020-01-08 11:52:28 +0000

    hotel passes hasName test

--- src/main/java/Hotel.java
+++ src/main/java/Hotel.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Hotel {
+
+    private ArrayList<Bedroom> bedrooms;
+    private ArrayList<ConferenceRoom> conferenceRooms;
+    private String name;
+
+    public Hotel(String name){
+        this.name = name;
+        this.bedrooms = new ArrayList<Bedroom>();
+        this.conferenceRooms = new ArrayList<ConferenceRoom>();
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class HotelTest {
+
+    Hotel hotel;
+
+    @Before
+    public void before(){
+        hotel = new Hotel("CodeClan Towers");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("CodeClan Towers", hotel.getName());
+    }
 }
