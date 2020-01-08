91799488fa4c429e4c2e4a83382920bca25d608f
Cameron <cameron.beckit@gmail.com>
2020-01-08 11:25:05 +0000

    bedroom tests pass

--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BedroomTest {
+
+    Bedroom room;
+
+    @Before
+    public void before(){
+        room = new Bedroom(101,2, "Double");
+    }
+
+    @Test
+    public void hasRoomNumber(){
+        assertEquals(101, room.getRoomNumber());
+    }
 }
