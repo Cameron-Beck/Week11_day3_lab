9ac89488a2a24c69050e1c100dfb5a12ce960e6f
Cameron <cameron.beckit@gmail.com>
2020-01-08 11:38:21 +0000

    add test to Conference Room Test file

--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class ConferenceRoomTest {
+
+    ConferenceRoom conferenceRoom;
+
+    @Before
+    public void before(){
+      conferenceRoom = new ConferenceRoom(10, "The Funky Sweet Suite");
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(10, conferenceRoom.getCapacity());
+    }
+
 }
