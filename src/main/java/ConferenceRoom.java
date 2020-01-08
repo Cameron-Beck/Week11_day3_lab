48718637650862f494799fcf63a79c784f12e4a7
Cameron <cameron.beckit@gmail.com>
2020-01-08 11:43:25 +0000

    conference room passes capacity test

--- src/main/java/ConferenceRoom.java
+++ src/main/java/ConferenceRoom.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class ConferenceRoom {
+
+    private int capacity;
+    private String name;
+    private ArrayList<Guest> guests;
+
+    public ConferenceRoom(int capacity, String name){
+        this.capacity = capacity;
+        this.name = name;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
 }
