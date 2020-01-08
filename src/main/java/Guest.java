f77a8ff09a002557eeab934fba8bdcd395145e55
Cameron <cameron.beckit@gmail.com>
2020-01-08 11:15:24 +0000

    initial commit

---
+++ src/main/java/Guest.java
@@ -0,0 +1,20 @@
+public class Guest {
+
+    private String name;
+    private char gender;
+
+    public Guest(String name,char gender){
+        this.name = name;
+        this.gender = gender;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+}
