b394ab2299afc2962f1aede3d0a36d9a254f0300
Cameron <cameron.beckit@gmail.com>
2020-01-08 13:42:09 +0000

    Hotel passes check out from bedroom test

--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -9,12 +9,15 @@ import static org.junit.Assert.assertTrue;
 public class BedroomTest {
 
     Bedroom room;
-    Guest guest;
+    Guest guest1;
+    Guest guest2;
+
 
     @Before
     public void before(){
         room = new Bedroom(101,2, "Double");
-        guest = new Guest("Steven", 'M');
+        guest1 = new Guest("Steven", 'M');
+        guest2 = new Guest("Stephen", 'M');
     }
 
     @Test
@@ -39,14 +42,23 @@ public class BedroomTest {
 
     @Test
     public void canAddGuests(){
-        room.addGuest(guest);
+        room.addGuest(guest1);
         assertEquals(1, room.getGuests().size());
     }
 
     @Test
     public void canCheckCapacityAfterAddingGuest(){
-        room.addGuest(guest);
+        room.addGuest(guest1);
         assertEquals(1, room.checkCapacity());
+    }
+
+    @Test
+    public void canRemoveGuests(){
+        room.addGuest(guest1);
+        room.addGuest(guest2);
+        room.removeGuest(guest1);
+        assertEquals(1, room.getGuests().size());
+        assertTrue(room.getGuests().contains(guest2));
     }
 }
 
