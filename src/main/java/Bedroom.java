7c3cdf5c2107da75160eb0121e987d42124ec7fa
Cameron <cameron.beckit@gmail.com>
2020-01-08 11:21:41 +0000

    adds methods to bedroom

--- src/main/java/Bedroom.java
+++ src/main/java/Bedroom.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class Bedroom {
+
+    private int roomNumber;
+    private int capacity;
+    private ArrayList<Guest> guests;
+    private String type;
+
+    public Bedroom(int roomNumber, int capacity, String type){
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        this.type = type;
+        this.guests = new ArrayList<Guest>();
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getCapacity(){
+        return capacity;
+    }
+
+    public ArrayList<Guest> getGuests() {
+        return new ArrayList<Guest>(guests);
+    }
+
+    public String getType() {
+        return type;
+    }
 }
