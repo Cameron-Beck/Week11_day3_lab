de64b02e4d256409375bfdaf6187cef647e88b34
Cameron <cameron.beckit@gmail.com>
2020-01-08 13:47:07 +0000

    adds canRemoveGuests teest to ConferenceRoomTest

--- src/test/java/ConferenceRoomTest.java
+++ src/test/java/ConferenceRoomTest.java
@@ -2,16 +2,19 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 public class ConferenceRoomTest {
 
     ConferenceRoom conferenceRoom;
     Guest guest1;
+    Guest guest2;
 
     @Before
     public void before(){
       conferenceRoom = new ConferenceRoom(10, "The Funky Sweet Suite");
       guest1 = new Guest("Steven", 'M');
+        guest1 = new Guest("Stephen", 'M');
     }
 
     @Test
@@ -39,5 +42,14 @@ public class ConferenceRoomTest {
     public void canCheckCapacity(){
         conferenceRoom.addGuest(guest1);
         assertEquals(9, conferenceRoom.checkCapacity());
+    }
+
+    @Test
+    public void canRemoveGuests(){
+        conferenceRoom.addGuest(guest1);
+        conferenceRoom.addGuest(guest2);
+        conferenceRoom.removeGuest(guest1);
+        assertEquals(1, conferenceRoom.getGuests().size());
+        assertTrue(conferenceRoom.getGuests().contains(guest2));
     }
 }
